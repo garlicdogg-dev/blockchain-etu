d7f32553d49e424c2d143b6992f7168eb1dae146
Ilya Kovalchuk <garlicdogg@gmail.com>
2025-12-24 13:40:36 +0300

    last task done

--- lab2/Task_04.sol
+++ lab2/Task_04.sol
@@ -1,32 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Task_02 {
-    uint[] public dynamicArray; // Динамический массив
-    uint[5] public fixedArray;  // Фиксированный массив из 5 элементов
+contract Task_04 {
+    mapping(address => uint256) public balances; // Маппинг адресов на балансы
 
-    function addToDynamicArray(uint _value) external {
-        dynamicArray.push(_value); // Добавление элемента в динамический массив
+    function setBalance(address _addr, uint256 _balance) external {
+        balances[_addr] = _balance; // Установка баланса для указанного адреса
     }
 
-    function sumArray() public view returns (uint) {
-        uint sum = 0;
-
-        for (uint i = 0; i < dynamicArray.length; i++) {
-            sum += dynamicArray[i];
-        }
-
-        return sum;
+    function getBalance(address _addr) public view returns (uint256) {
+        return balances[_addr];
     }
 
-    // Новая функция: генерация массива квадратов чисел от 1 до n
-    function generateSquares(uint n) public pure returns (uint[] memory) {
-        uint[] memory squares = new uint[](n);
+    // Новая функция: первый элемент Фибоначчи, который превышает n
+    function generateFibo(uint n) public pure returns (uint256) {
+        uint256 a = 0;
+        uint256 b = 1;
+        uint256 fib = 1;
 
-        for (uint i = 0; i < n; i++) {
-            squares[i] = (i + 1) * (i + 1);
-        }
+        do {
+            fib = a + b;
+            a = b;
+            b = fib;
+        } while (fib <= n);
 
-        return squares;
+        return fib;
     }
 }
