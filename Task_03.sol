0506b0a67231f539cd713aa424c150854c201626
Ilya Kovalchuk <garlicdogg@gmail.com>
2025-12-24 12:43:30 +0300

    task3 done

---
+++ Task_03.sol
@@ -0,0 +1,19 @@
+contract Task_03 {
+   struct Person {
+       string name;
+       uint8 age;
+   }
+
+
+   Person[] public people; // Массив структур
+
+
+   function addPerson(string memory _name, uint8 _age) external {
+       people.push(Person(_name, _age)); // Добавление новой структуры в массив
+   }
+   
+   function getUser(uint index) public view returns (string memory, uint8) {
+    Person memory person = people[index];
+    return (person.name, person.age);
+}
+}
